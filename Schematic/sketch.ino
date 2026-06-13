5aa18c9ec0bf42a950c8ac0225f8525b203b08b5
robisptraa <robibaliksaputra123@gmail.com>
2026-06-13 20:30:17 +0700

    initial_commit

---
+++ Schematic/sketch.ino
@@ -0,0 +1,15 @@
+
+
+int relay_1 = 2;
+int relay_2 = 4;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+}
+
+void loop() {
+  
+  Serial_Println("Relay 1 and Relay 2 are OFF");
+}
